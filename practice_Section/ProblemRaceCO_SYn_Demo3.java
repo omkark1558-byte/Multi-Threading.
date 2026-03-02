f5514369c4afaab5022a3ea8c8bb363f5b87b138
omkark1558-byte <omkark1558@gmail.com>
2026-03-02 23:47:11 +0530

    Modified

--- practice_Section/ProblemRaceCO_SYn_Demo3.java
+++ practice_Section/ProblemRaceCO_SYn_Demo3.java
@@ -4,51 +4,52 @@ public class ProblemRaceCO_SYn_Demo3 {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		BankDemo Task = new BankDemo();
-		
-		First_Thread Task_1 = new First_Thread(Task);
-		Second_Thread Task_2 = new Second_Thread(Task);
-	
-		
+		Bank_Account Task = new Bank_Account();
+		Bank_Account Task_3 = new Bank_Account();
+
+		First_Thread Task_1 = new First_Thread();
+		Second_Thread Task_2 = new Second_Thread();
+		Task_1.count = Task;
+		Task_2.count = Task_3;
+
 		Task_1.start();
 		Task_2.start();
-		
+
 		Task_1.join();
 		Task_2.join();
-		
+
 		System.out.println(Task.getCount());
-		System.out.println(Task.getCount());
+		System.out.println(Task_3.getCount());
 	}
 
 }
 
 class Bank_Account {
-	int Count;
+	int Count = 0;
 
-	public void Incress() {
+	public synchronized void Incress() {
 		this.Count++;
 	}
 
 	public int getCount() {
-		return Count;
+		return this.Count;
 	}
 }
 
 class First_Thread extends Thread {
 
-	BankDemo count;
+	Bank_Account count;
 
-	public First_Thread(BankDemo count) {
-		this.count = count;
-	}
+//	public First_Thread(Bank_Account count) {
+//		this.count = count;
+//	}
 
 	public void run() {
 		try {
 			Thread.sleep(1500);
-			}catch(Exception e)
-			{
-				System.out.println(e);
-			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		for (int i = 0; i < 1000; i++) {
 			count.Incress();
 		}
@@ -57,19 +58,18 @@ class First_Thread extends Thread {
 }
 
 class Second_Thread extends Thread {
-	BankDemo count;
+	Bank_Account count;
 
-	public Second_Thread(BankDemo count) {
-		this.count = count;
-	}
+//	public Second_Thread(Bank_Account count) {
+//		this.count = count;
+//	}
 
 	public void run() {
 		try {
 			Thread.sleep(1500);
-			}catch(Exception e)
-			{
-				System.out.println(e);
-			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		for (int j = 0; j < 1500; j++) {
 			count.Incress();
 		}
